53c11198af17e835b1e7d5218afcbe0e97fbe344
jsnimda <7615255+jsnimda@users.noreply.github.com>
2024-09-25 09:24:28 +0800

    feat(transform): support more zod types

--- src/transform.ts
+++ src/transform.ts
@@ -8,6 +8,11 @@ const isValidNumber = (str: string): boolean => {
   return !isNaN(Number(str)) && isFinite(Number(str));
 };
 
+const isValidDate = (str: string): boolean => {
+  const isoDateRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d{0,3})?Z$/;
+  return isValidNumber(str) || isoDateRegex.test(str);
+};
+
 export class UnsupportedZodType extends Error {
   constructor(typeName: string) {
     super(`Unsupported schema type for query coercion: ${typeName}`);
@@ -17,16 +22,25 @@ export class UnsupportedZodType extends Error {
 
 export function transformSchema(schema: z.ZodTypeAny): z.ZodTypeAny {
   if (isZodType(schema, 'ZodEffects')) {
+    if (schema._def.effect.type === 'preprocess') {
+      return schema;
+    }
     schema._def.schema = transformSchema(schema._def.schema);
     return schema;
   }
 
-  if (isZodType(schema, 'ZodBoolean')) {
+  if (isZodType(schema, 'ZodLiteral')) {
+    if (typeof schema._def.value === 'symbol') throw new UnsupportedZodType('ZodLiteral with symbol');
     return z.preprocess((val) => {
-      if (val === 'true' || val === '1') return true;
-      if (val === 'false' || val === '0') return false;
+      if (typeof schema._def.value === 'symbol') return val;
+      if (typeof schema._def.value === 'undefined') return val;
+      if (('' + schema._def.value) === val) return schema._def.value;
       return val;
     }, schema);
+  }
+
+  if (isZodType(schema, 'ZodString')) {
+    return schema;
   }
 
   if (isZodType(schema, 'ZodNumber')) {
@@ -36,11 +50,34 @@ export function transformSchema(schema: z.ZodTypeAny): z.ZodTypeAny {
     }, schema);
   }
 
-  if (isZodType(schema, 'ZodString')) {
+  if (isZodType(schema, 'ZodBigInt')) {
     return z.preprocess((val) => {
-      if (typeof val === 'string') return val;
+      try {
+        if (typeof val === 'string') return BigInt(val);
+        return val;
+      } catch {
+        return val;
+      }
+    }, schema);
+  }
+
+  if (isZodType(schema, 'ZodBoolean')) {
+    return z.preprocess((val) => {
+      if (val === 'true') return true;
+      if (val === 'false') return false;
       return val;
     }, schema);
+  }
+
+  if (isZodType(schema, 'ZodDate')) {
+    return z.preprocess((val) => {
+      if (typeof val === 'string' && isValidDate(val)) return new Date(val);
+      return val;
+    }, schema);
+  }
+
+  if (isZodType(schema, 'ZodEnum')) {
+    return schema;
   }
 
   if (isZodType(schema, 'ZodNull')) {
@@ -63,22 +100,12 @@ export function transformSchema(schema: z.ZodTypeAny): z.ZodTypeAny {
     return schema;
   }
 
-  if (isZodType(schema, 'ZodDefault')) {
-    schema._def.innerType = transformSchema(schema._def.innerType);
-    return schema;
-  }
-
   if (isZodType(schema, 'ZodArray')) {
     schema._def.type = transformSchema(schema._def.type);
     return z.preprocess((val) => {
       if (Array.isArray(val)) return val;
       return [val];
     }, schema);
-  }
-
-  if (isZodType(schema, 'ZodUnion')) {
-    schema._def.options = schema._def.options.map(transformSchema) as any;
-    return schema;
   }
 
   if (isZodType(schema, 'ZodTuple')) {
@@ -90,6 +117,60 @@ export function transformSchema(schema: z.ZodTypeAny): z.ZodTypeAny {
       if (Array.isArray(val)) return val;
       return [val];
     }, schema);
+  }
+
+  if (isZodType(schema, 'ZodUnion')) {
+    schema._def.options = schema._def.options.map(transformSchema) as any;
+    return schema;
+  }
+
+  if (isZodType(schema, 'ZodSet')) {
+    schema._def.valueType = transformSchema(schema._def.valueType);
+    return z.preprocess((val) => {
+      if (Array.isArray(val)) return new Set(val);
+      return new Set([val]);
+    }, schema);
+  }
+
+  if (isZodType(schema, 'ZodIntersection')) {
+    schema._def.left = transformSchema(schema._def.left);
+    schema._def.right = transformSchema(schema._def.right);
+    return schema;
+  }
+
+  if (isZodType(schema, 'ZodCatch')) {
+    schema._def.innerType = transformSchema(schema._def.innerType);
+    return schema;
+  }
+
+  if (isZodType(schema, 'ZodDefault')) {
+    schema._def.innerType = transformSchema(schema._def.innerType);
+    return schema;
+  }
+
+  if (isZodType(schema, 'ZodPipeline')) {
+    schema._def.in = transformSchema(schema._def.in);
+    return schema;
+  }
+
+  if (isZodType(schema, 'ZodUndefined')) {
+    return schema;
+  }
+
+  if (isZodType(schema, 'ZodVoid')) {
+    return schema;
+  }
+
+  if (isZodType(schema, 'ZodAny')) {
+    return schema;
+  }
+
+  if (isZodType(schema, 'ZodUnknown')) {
+    return schema;
+  }
+
+  if (isZodType(schema, 'ZodNever')) {
+    return schema;
   }
 
   // If it's not a handled type, throw error
--- src/zod-types.ts
+++ src/zod-types.ts
@@ -3,18 +3,42 @@ import type { z } from 'zod';
 // Reference: https://github.com/samchungy/zod-openapi/blob/master/src/zodType.ts
 
 type ZodTypeMap = {
+  /* unsupported */ ZodBranded: z.ZodBranded<z.ZodTypeAny, string | number | symbol>;
+  /* unsupported */ ZodDiscriminatedUnion: z.ZodDiscriminatedUnion<string, any>;
+  /* unsupported */ ZodFunction: z.ZodFunction<z.ZodTuple<any, any>, z.ZodTypeAny>;
+  /* unsupported */ ZodLazy: z.ZodLazy<z.ZodTypeAny>;
+  /* unsupported */ ZodMap: z.ZodMap;
+  /* unsupported */ ZodNaN: z.ZodNaN;
+  /* unsupported */ ZodNativeEnum: z.ZodNativeEnum<z.EnumLike>;
+  /* unsupported */ ZodPromise: z.ZodPromise<z.ZodTypeAny>;
+  /* unsupported */ ZodReadonly: z.ZodReadonly<z.ZodTypeAny>;
+  /* unsupported */ ZodRecord: z.ZodRecord;
+  /* unsupported */ ZodSymbol: z.ZodSymbol;
+  /* unsupported, except as the top-level schema */ ZodObject: z.ZodObject<z.ZodRawShape>;
+  ZodAny: z.ZodAny;
   ZodArray: z.ZodArray<z.ZodTypeAny>;
+  ZodBigInt: z.ZodBigInt;
   ZodBoolean: z.ZodBoolean;
+  ZodCatch: z.ZodCatch<z.ZodTypeAny>;
+  ZodDate: z.ZodDate;
   ZodDefault: z.ZodDefault<z.ZodTypeAny>;
   ZodEffects: z.ZodEffects<z.ZodTypeAny>;
+  ZodEnum: z.ZodEnum<[string, ...string[]]>;
+  ZodIntersection: z.ZodIntersection<z.ZodTypeAny, z.ZodTypeAny>;
+  ZodLiteral: z.ZodLiteral<z.Primitive>;
+  ZodNever: z.ZodNever;
   ZodNull: z.ZodNull;
   ZodNullable: z.ZodNullable<z.ZodTypeAny>;
   ZodNumber: z.ZodNumber;
-  ZodObject: z.ZodObject<z.ZodRawShape>;
   ZodOptional: z.ZodOptional<z.ZodTypeAny>;
+  ZodPipeline: z.ZodPipeline<z.ZodTypeAny, z.ZodTypeAny>;
+  ZodSet: z.ZodSet;
   ZodString: z.ZodString;
   ZodTuple: z.AnyZodTuple;
+  ZodUndefined: z.ZodUndefined;
   ZodUnion: z.ZodUnion<z.ZodUnionOptions>;
+  ZodUnknown: z.ZodUnknown;
+  ZodVoid: z.ZodVoid;
 };
 
 export function isZodType<K extends keyof ZodTypeMap>(
